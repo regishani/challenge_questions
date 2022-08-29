a5b6ca805fe35a56bb3f230a2d2a9a388e2f3bad
Regina Zhyldyzbekova <regina.zhyldyzbekova@gmail.com>
2022-08-28 23:01:05 -0400

    adding coding challenge

---
+++ palindrome.js
@@ -0,0 +1,20 @@
+/** Return true if a given string is a palindrome and return false if it
+ * is not. Palindromes are strings that form the exact same string when reversed.
+ * Make sure to include spaces and punctuation when determining if the given
+ * string is a palindrome.
+ *
+ * @example
+ * palindrome('abba') === true
+ * palindrome('abcba') === true
+ * palindrome('frontend') === false
+ */
+
+ const palindrome = (str) => {
+    //Check if first string and last string are the same
+    return str === reversedString(str)
+};
+const reversedString = (str) =>{
+    return str.split('').reverse().join('')
+}
+
+module.exports = palindrome;
